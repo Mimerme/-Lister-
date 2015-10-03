d6d830f88caa6481a7ba1079a810593d0916c2ec
Mimerme <androssdragonq.9@gmail.com>
2015-10-03 18:53:53 -0400

    Added complete twilio integration, get ready for mongodb

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var client = require('twilio')('ACCOUNT_SID', 'AUTH_TOKEN');
 var app = express();
 
 
@@ -8,7 +9,6 @@ var NUMBER  = "(732) 333-6592";
 
 app.get('/', function (req, res){
   ///Placeholder for site
-  console.log('reset');
   res.send("There's nothing here, plz help");
 });
 
@@ -17,21 +17,56 @@ app.get('/fetch', function(req, res){
   var number = req.query.phoneNum;
 });
 
-app.get('/messageRecieve', function(req, res){
-  console.log(req.query);
-  res.send('requrest');
-});
-
 //Adds song to mongodb server
 //Takes in phone number and song / search term
 app.get('/addSong', function(req, res){
-  var number = req.query.phoneNum;
-  var songName = req.query.song;
+  var number = req.query.From;
+  var text = req.query.Body;
+
+  var songName = parseBody(text);
+
+  //Invalid syntax
+  if(songName == null){
+    sendMessage(number, "The syntax of your request was invalid");
+    res.send("Reponse Finished: With Error by user");
+  }
+
+  console.log("adding " + songName);
   var songURL = getSongUrl(songName);
+  addToDatabase(From, songURL);
+  res.send("Reponse Finished: With Success");
 });
+
+//Send a message through twilio
+function sendMessage(number, message){
+  client.sendMessage({
+
+    to:number, // Any number Twilio can deliver to
+    from: NUMBER, // A number you bought from Twilio and can use for outbound communication
+    body: message // body of the SMS message
+
+}, function(err, responseData) { //this function is executed when a response is received from Twilio
+
+    if (!err) {
+        console.log(responseData.from);
+        console.log(responseData.body);
+    }
+});
+}
+
+function parseBody(body){
+  if(body.indexOf("add ") == -1){
+    return null;
+  }
+  return body.replace("add ", "");
+}
 
 function getSongUrl(searchTerm){
   //Search for song using youtube api
+}
+
+function addToDatabase(number, URL){
+  //Add the url with teh number to the song database
 }
 
 app.listen(PORT);
