26520e1f2a110f823d1cf9cf8fd7817c84f6f0e9
Mimerme <androssdragonq.9@gmail.com>
2015-10-03 17:08:56 -0400

    First Commit

---
+++ index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var app = express();
+
+
+var PORT = 3000;
+var API_KEY;
+var NUMBER  = "(732) 333-6592";
+
+app.get('/', function (req, res){
+  ///Placeholder for site
+  res.send("There's nothing here, plz help");
+});
+
+app.get('/fetch', function(req, res){
+  //fetches all songs for the given url
+  var number = req.query.phoneNum;
+});
+
+app.get('/messageRecieve', function(req, res){
+
+});
+
+//Adds song to mongodb server
+//Takes in phone number and song / search term
+app.get('/addSong', function(req, res){
+  var number = req.query.phoneNum;
+  var songName = req.query.song;
+  var songURL = getSongUrl(songName);
+});
+
+function getSongUrl(searchTerm){
+  //Search for song using youtube api
+}
+
+app.listen(PORT);
+console.log("The server is now listening on port " + PORT);
